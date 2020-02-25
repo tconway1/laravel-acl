0ef45a0be4f4a1c4c59c74bba0973419965e9563
tconway1 <56689274+tconway1@users.noreply.github.com>
2020-02-25 07:14:28 -0500

    update model keyType

--- src/VivifyIdeas/Acl/Models/Group.php
+++ src/VivifyIdeas/Acl/Models/Group.php
@@ -17,5 +17,17 @@ class Group extends Model
 
     public $timestamps = false;
 
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
     public $incrementing = false;
+	
+	/**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
 }
