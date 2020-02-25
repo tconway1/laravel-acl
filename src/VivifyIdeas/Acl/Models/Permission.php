a6ac185844834df039f14ef23f8774799edf10f7
tconway1 <56689274+tconway1@users.noreply.github.com>
2020-02-25 07:15:06 -0500

    update model keyType

--- src/VivifyIdeas/Acl/Models/Permission.php
+++ src/VivifyIdeas/Acl/Models/Permission.php
@@ -16,5 +16,17 @@ class Permission extends Model
 
     public $timestamps = false;
 
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
     public $incrementing = false;
+	
+	/**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
 }
